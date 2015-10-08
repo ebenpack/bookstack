42e97435b685d443219bfd62507d27169f269cef
ebenpack <ebenpackwood@gmail.com>
2015-10-08 19:47:20 -0400

    Add readstate/token actions

--- bookstack/static/bookstack/develop/js/react/actions/StackDetailActions.js
+++ bookstack/static/bookstack/develop/js/react/actions/StackDetailActions.js
@@ -3,6 +3,9 @@ var Reflux = require('reflux');
 var StackDetailActions = Reflux.createActions({
     'loadStack': {asyncResult: true},
     'viewStackDetail': {},
+    'updateRead': {},
+    'setReadState': {},
+    'setToken': {},
 });
 
 module.exports = StackDetailActions;
