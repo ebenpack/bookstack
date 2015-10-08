1e53af68676ae8cf17d084e2eb170be683508dfd
ebenpack <ebenpackwood@gmail.com>
2015-10-08 19:49:36 -0400

    Add read toggling

--- bookstack/static/bookstack/develop/js/react/components/BookStack.jsx
+++ bookstack/static/bookstack/develop/js/react/components/BookStack.jsx
@@ -3,7 +3,12 @@ var React = require('react');
 var Book = require('./Book.jsx');
 var Category = require('./Category.jsx');
 
+var StackDetailActions = require('../actions/StackDetailActions');
+
 var BookStack = React.createClass({
+    handleChange: function(e){
+        StackDetailActions.setReadState(this.props.data.id, e.target.checked);
+    },
     render: function() {
         var staticPath = this.props.staticPath;
         var classString = 'bookstack';
@@ -13,17 +18,17 @@ var BookStack = React.createClass({
         var categories = (this.props.data.categories && this.props.data.categories.length > 0);
         return (
             <div className={classString} >
-                <div className="position" contentEditable="true">{this.props.data.position}</div>
+                <div className="position">{this.props.data.position}</div>
                 <Book data={this.props.data.book} staticPath={staticPath} />
                 <div className="info">
                     {categories ? 'Categories: ' : ''}
                     <ul className="categories">
                         {this.props.data.categories.map(function(category, i) {
-                            return <Category key={i} data={category} />
+                            return (<Category key={i} data={category} />);
                         })}
                     </ul>
                     <div className="read">
-                        Read: <input type="checkbox" checked={this.props.data.read}  />
+                        Read: <input onChange={this.handleChange} type="checkbox" checked={this.props.data.read}  />
                     </div>
                 </div>
             </div>
