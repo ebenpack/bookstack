32870ca90f6b8260b00f3de6ff0a753c9a9ab6d2
ebenpack <ebenpackwood@gmail.com>
2015-10-10 11:34:47 -0400

    Remove redundant stack link handling

--- bookstack/static/bookstack/develop/js/react/components/Stack.jsx
+++ bookstack/static/bookstack/develop/js/react/components/Stack.jsx
@@ -6,19 +6,13 @@ var StackListActions = require('../actions/StackListActions.js');
 var StackDetailActions = require('../actions/StackDetailActions.js');
 
 var Stack = React.createClass({
-    handleClick: function(event) {
-        event.preventDefault();
-        StackDetailActions.loadStack(event.target.dataset.id);
-    },
     render: function() {
         var staticPath = this.props.staticPath;
         var id = this.props.data.id;
         return (
             <div className="stack">
                 <h1 className="stackName">
-                    <a onClick={this.handleClick}>
-                        <Link to={"/list/" + id}>{this.props.data.name}</Link>
-                    </a>
+                    <Link to={"/list/" + id}>{this.props.data.name}</Link>
                 </h1>
                 <div className="user">{this.props.data.user}</div>
                 <div className="creationDate">{this.props.data.creation_date}</div>
