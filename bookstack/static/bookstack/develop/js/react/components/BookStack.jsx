8cabe396a497fce113b2f50b9a7a682e7f3abab4
ebenpack <ebenpackwood@gmail.com>
2015-10-11 11:37:22 -0400

    Add reordering support

--- bookstack/static/bookstack/develop/js/react/components/BookStack.jsx
+++ bookstack/static/bookstack/develop/js/react/components/BookStack.jsx
@@ -6,19 +6,59 @@ var Category = require('./Category.jsx');
 var StackDetailActions = require('../actions/StackDetailActions');
 
 var BookStack = React.createClass({
-    handleChange: function(e) {
+    getInitialState: function() {
+        return {
+            editing: false,
+        };
+    },
+    toggleRead: function(e) {
         StackDetailActions.setReadState(this.props.data.id, e.target.checked);
     },
+    setEditingStateOn: function() {
+        this.setState({
+            editing: true,
+        });
+    },
+    setEditingStateOff: function() {
+        this.setState({
+            editing: false,
+        });
+    },
+    updatePosition: function(e) {
+        var position = parseInt(e.target.value, 10);
+        this.setEditingStateOff();
+        if (position && this.props.data.position !== position) {
+            StackDetailActions.setPosition(this.props.data.id, this.props.data.position, position);
+        }
+    },
     render: function() {
+        var context = this;
         var staticPath = this.props.staticPath;
         var classString = 'bookstack';
         if (this.props.data.read) {
             classString += ' isRead';
         }
         var categories = (this.props.data.categories && this.props.data.categories.length > 0);
+        var position = (
+            this.state.editing ?
+                (
+                    <input
+                        className="position"
+                        onBlur={this.updatePosition}
+                        defaultValue={this.props.data.position}
+                        onMouseOut={this.setEditingStateOff} />
+                ) :
+                (
+                    <div
+                        className="position"
+                        onClick={this.setEditingStateOn}>
+                        {this.props.data.position}
+                    </div>
+                )
+        );
         return (
             <div className={classString} >
-                <div className="position">{this.props.data.position}</div>
+                {position}
                 <Book data={this.props.data.book} staticPath={staticPath} />
                 <div className="info">
                     {categories ? 'Categories: ' : ''}
@@ -28,7 +68,7 @@ var BookStack = React.createClass({
                         })}
                     </ul>
                     <div className="read">
-                        Read: <input onChange={this.handleChange} type="checkbox" checked={this.props.data.read}  />
+                        Read: <input onChange={this.toggleRead} type="checkbox" checked={this.props.data.read}  />
                     </div>
                 </div>
             </div>
