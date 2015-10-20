69f267c69d0c69ee5e72cd5a5de581b16e23edec
ebenpack <ebenpackwood@gmail.com>
2015-10-19 20:21:20 -0400

    Add AddBook component

---
+++ bookstack/static/bookstack/develop/js/react/components/AddBook.jsx
@@ -0,0 +1,61 @@
+var React = require('react');
+var Reflux = require('reflux');
+
+var AddBookActions = require('../actions/AddBookActions');
+var AddBookStore = require('../stores/AddBookStore');
+
+var AddBook = React.createClass({
+    mixins: [Reflux.connect(AddBookStore)],
+    getInitialState: function() {
+        return {
+            title: '',
+            booksAutocomplete: [],
+        };
+    },
+    handleChange: function(e) {
+        var search = e.target.value;
+        if (search) {
+            AddBookActions.searchBooks(search);
+        }
+    },
+    selectBook: function(id, e){
+        // TODO: Do something here
+        // Get this book, and autofill the form fields
+    },
+    render: function() {
+        var autocompleteResults = "";
+        if (this.state.booksAutocomplete.length > 0) {
+            autocompleteResults = (
+                <ul className="autocomplete">
+                    {this.state.booksAutocomplete.map(function(suggestion){
+                        return (<li key={suggestion.id} onClick={this.selectBook.bind(this, suggestion.id)}>{suggestion.title}</li>);
+                    }, this)}
+                </ul>
+            );
+        }
+        return (
+            <div>
+            <input type="text" title={this.state.title} onChange={this.handleChange} />
+            {autocompleteResults}
+            </div>
+        );
+    }
+// read (boolean),
+// position (integer),
+// book (BookSerializer),
+// categories (array[string]),
+// stack (string),
+// id (integer)
+
+// title (string),
+// pages (integer),
+// isbn (string),
+// img (string),
+// authors (array[AuthorSerializer]),
+// publishers (array[PublisherSerializer]),
+// id (integer)
+// AuthorSerializer
+// PublisherSerializer
+});
+
+module.exports = AddBook;
