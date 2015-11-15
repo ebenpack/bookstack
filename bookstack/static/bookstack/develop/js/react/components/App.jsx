ef3b4c8dd12ca008ad5cb46c3c83470d4f08dd5b
ebenpack <ebenpackwood@gmail.com>
2015-11-15 09:32:51 -0500

    Add book search to menu bar

--- bookstack/static/bookstack/develop/js/react/components/App.jsx
+++ bookstack/static/bookstack/develop/js/react/components/App.jsx
@@ -1,16 +1,22 @@
 var React = require('react');
 var Reflux = require('reflux');
 
+var History = require('react-router').History;
+
 var AppStore = require('../stores/AppStore');
 var AppActions = require('../actions/AppActions');
 
 var Link = require('react-router').Link;
 
 var App = React.createClass({
-    mixins: [Reflux.connect(AppStore)],
+    mixins: [
+        Reflux.connect(AppStore),
+        History
+    ],
     getInitialState: function() {
         return {
             token: '',
+            bookSearch: '',
         };
     },
     componentDidMount: function() {
@@ -20,6 +26,16 @@ var App = React.createClass({
         e.preventDefault();
         e.stopPropagation();
         AppActions.logoff();
+    },
+    handleSearch: function(e) {
+        this.setState({
+            bookSearch: e.target.value,
+        });
+    },
+    handleKeyUp: function(e) {
+        if (e.key === 'Enter') {
+            this.history.pushState({foobar: "baz"}, '/addbook');
+        }
     },
     render: function() {
         var loggedIn = this.state.token !== '';
@@ -32,8 +48,16 @@ var App = React.createClass({
             <div className="container">
                 <ul className="row menu">
                     <li><Link to={"/list"}>View Stacks</Link></li>
+                    <li>
+                        <label>
+                            Add Book <input
+                                type="text"
+                                title={this.state.bookSearch}
+                                onChange={this.handleSearch}
+                                onKeyUp={this.handleKeyUp} />
+                        </label>
+                    </li>
                     {login}
-                    <li><Link to={"/addbook"}>Add Book</Link></li>
                 </ul>
                 {this.props.children}
             </div>
