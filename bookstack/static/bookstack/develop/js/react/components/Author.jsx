b5a5d9201f7d33c78f72fd7b1212fc6240df8b2c
ebenpack <ebenpackwood@gmail.com>
2015-11-17 17:55:51 -0500

    Make author link optional

--- bookstack/static/bookstack/develop/js/react/components/Author.jsx
+++ bookstack/static/bookstack/develop/js/react/components/Author.jsx
@@ -3,11 +3,14 @@ var Link = require('react-router').Link;
 
 var Author = React.createClass({
     render: function() {
+        var author = this.props.author.id ?
+            <Link to={"/author/" + this.props.author.id}>
+                {this.props.author.name}
+            </Link> :
+            this.props.author.name;
         return (
             <li className="author">
-                <Link to={"/author/" + this.props.author.id}>
-                    {this.props.author.name}
-                </Link>
+                {author}
             </li>
         );
     }
