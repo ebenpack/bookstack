7b7257295d57b5d84e4335befd894285b85a8558
ebenpack <ebenpackwood@gmail.com>
2017-01-29 10:54:41 -0500

    Add webpack

---
+++ bookstack/static/bookstack/develop/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: ['./src/main.js'],
+
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: '/assets/',
+        filename: 'bundle.js',
+    },
+
+    plugins: process.env.NODE_ENV === 'production' ? [
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin()
+        ] : [
+            new webpack.OldWatchingPlugin()
+        ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ],
+        sassLoader: {
+            sourceMap: true,
+            includePaths: [path.resolve(__dirname, "sass")]
+        }
+    }
+}
