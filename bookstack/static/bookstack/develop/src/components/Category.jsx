350534e4ec2dfe6ddf4ac1f12efd91cd465e2ef2
ebenpack <ebenpackwood@gmail.com>
2017-01-30 18:30:43 -0500

    Fix category access

--- bookstack/static/bookstack/develop/src/components/Category.jsx
+++ bookstack/static/bookstack/develop/src/components/Category.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Category(props){
     const onClick = props.onClick ? e=>props.onClick(e, props.category.get('id')) : ()=>{};
     return (
-        <li key={props.category.id} className="category" onClick={onClick}>
-            {props.category.category}
+        <li key={props.category.get('id')} className="category" onClick={onClick}>
+            {props.category.getIn(['detail', 'category'])}
         </li>
     );
 };
