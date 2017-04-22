2f6324100a75a784a15a7333bc67f925dd75f6e6
ebenpack <ebenpackwood@gmail.com>
2017-04-22 12:10:13 -0400

    Add managers

--- bookstack/models.py
+++ bookstack/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 from django.db.models import F
 
 
+class StackManager(models.Manager):
+    def create_stack(self, name, user, private=False, creation_date=None, books=None):
+        stack = self.create(name=name, user=user, private=private)
+        if creation_date is not None:
+            stack.creation_date = creation_date
+        if books is not None:
+            stack.books = books
+        stack.save()
+        return stack
+
+
 class Stack(models.Model):
     name = models.CharField(max_length=200)
     private = models.BooleanField(default=True)
@@ -12,13 +23,77 @@ class Stack(models.Model):
     )
     books = models.ManyToManyField('Book', through='BookStack')
 
+    objects = StackManager()
+
     def __str__(self):
         return self.name
+
+
+class BookManager(models.Manager):
+    def create_book(self, title, pages, isbn, img=None, authors=None, publishers=None):
+        book = self.create(title=title, pages=pages, isbn=isbn)
+        if img is not None:
+            book.img = img
+
+        # TODO: FIX?
+        if authors is not None:
+            book.save()
+            book.authors = authors.values_list('id', flat=True)
+        if publishers is not None:
+            book.save()
+            book.publishers = publishers.values_list('id', flat=True)
+        book.save()
+        return book
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    pages = models.IntegerField()
+    isbn = models.CharField(max_length=15, unique=True)
+    img = models.URLField(max_length=500, blank=True)
+    authors = models.ManyToManyField('Author')
+    publishers = models.ManyToManyField('Publisher')
+
+    objects = BookManager()
+
+    def __str__(self):
+        return self.title
+
+
+class BookStackManager(models.Manager):
+    def create_bookstack(self, book, stack, read=False, categories=None):
+        bookstack = self.create(
+            read=read,
+            book=book,
+            stack=stack,
+        )
+        if categories is not None:
+            bookstack.save()
+            for category in categories:
+                BookStackCategory.objects.create_bookstack_category(
+                    bookstack=bookstack,
+                    category=category
+                )
+
+        bookstack.save()
+        return bookstack
 
 
 class BookStack(models.Model):
     class Meta:
         ordering = ['position']
+
+    stack = models.ForeignKey(Stack)
+    book = models.ForeignKey(Book)
+    read = models.BooleanField(default=False)
+    categories = models.ManyToManyField(
+        'Category',
+        through='BookStackCategory',
+        blank=True
+    )
+    position = models.IntegerField(default=0, db_index=True)
+
+    objects = BookStackManager()
 
     def max_position(self):
         return self.stack.bookstack_set.count()
@@ -30,8 +105,11 @@ class BookStack(models.Model):
         '''
         from_position = self.position
         to_position = position
-
         bookstack_set = self.stack.bookstack_set
+        max_position = self.max_position()
+
+        if  to_position < 0 or to_position > max_position:
+            raise IndexError
 
         start = min(from_position, to_position)
         end = max(from_position, to_position)
@@ -57,39 +135,17 @@ class BookStack(models.Model):
         moved.position = to_position
         moved.save()
 
-    stack = models.ForeignKey(Stack)
-    book = models.ForeignKey('Book')
-    read = models.BooleanField(default=False)
-    categories = models.ManyToManyField(
-        'Category',
-        through='BookStackCategory',
-        blank=True
-    )
-    position = models.IntegerField(default=0, db_index=True)
-
     def toggle_read(self):
         self.read = not self.read
         self.save()
 
     def save(self, *args, **kwargs):
         if not self.position:
-            self.position = self.stack.bookstack_set.count() + 1
+            self.position = self.max_position() + 1
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.book.title
-    
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    pages = models.IntegerField()
-    isbn = models.CharField(max_length=15, unique=True)
-    img = models.URLField(max_length=500, blank=True)
-    authors = models.ManyToManyField('Author')
-    publishers = models.ManyToManyField('Publisher')
-
-    def __str__(self):
-        return self.title
 
 
 class Category(models.Model):
@@ -102,9 +158,18 @@ class Category(models.Model):
         return self.category
 
 
+class BookStackCategoryManager(models.Manager):
+    def create_bookstack_category(self, bookstack, category):
+        bookstack_category = self.create(bookstack=bookstack, category=category)
+        bookstack_category.save()
+        return bookstack_category
+
+
 class BookStackCategory(models.Model):
     bookstack = models.ForeignKey(BookStack)
     category = models.ForeignKey(Category)
+
+    objects = BookStackCategoryManager()
 
     class Meta:
         unique_together = (('category', 'bookstack'))
