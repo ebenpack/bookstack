a7f3ef77b1249d96afc3cfa880617a3eac672064
ebenpack <ebenpackwood@gmail.com>
2018-04-03 22:08:33 -0400

    Remove unused managers

--- bookstack/models.py
+++ bookstack/models.py
@@ -2,16 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import F, Case, When, Value, Q, ExpressionWrapper
 
-class StackManager(models.Manager):
-    def create_stack(self, name, user, private=False, creation_date=None, books=None):
-        stack = self.create(name=name, user=user, private=private)
-        if creation_date is not None:
-            stack.creation_date = creation_date
-        if books is not None:
-            stack.books = books
-        stack.save()
-        return stack
-
 
 class Stack(models.Model):
     name = models.CharField(max_length=200)
@@ -22,27 +12,8 @@ class Stack(models.Model):
     )
     books = models.ManyToManyField('Book', through='BookStack')
 
-    objects = StackManager()
-
     def __str__(self):
         return self.name
-
-
-class BookManager(models.Manager):
-    def create_book(self, title, pages, isbn, img=None, authors=None, publishers=None):
-        book = self.create(title=title, pages=pages, isbn=isbn)
-        if img is not None:
-            book.img = img
-
-        # TODO: FIX?
-        if authors is not None:
-            book.save()
-            book.authors = authors.values_list('id', flat=True)
-        if publishers is not None:
-            book.save()
-            book.publishers = publishers.values_list('id', flat=True)
-        book.save()
-        return book
 
 
 class Book(models.Model):
@@ -53,30 +24,8 @@ class Book(models.Model):
     authors = models.ManyToManyField('Author')
     publishers = models.ManyToManyField('Publisher')
 
-    objects = BookManager()
-
     def __str__(self):
         return self.title
-
-
-class BookStackManager(models.Manager):
-    def create_bookstack(self, book, stack, read=False, categories=None):
-        bookstack = self.create(
-            read=read,
-            book=book,
-            stack=stack,
-        )
-        if categories is None:
-            bookstack.save()
-        else:
-            bookstack.save()
-            for category in categories:
-                BookStackCategory.objects.create_bookstack_category(
-                    bookstack=bookstack,
-                    category=category
-                )
-
-        return bookstack
 
 
 class BookStack(models.Model):
@@ -92,8 +41,6 @@ class BookStack(models.Model):
         blank=True
     )
     position = models.IntegerField(default=0, db_index=True)
-
-    objects = BookStackManager()
 
     def max_position(self):
         return self.stack.bookstack_set.count()
@@ -138,6 +85,7 @@ class BookStack(models.Model):
                 output_field=models.IntegerField()
             )
         )
+        self.refresh_from_db()
 
     def toggle_read(self):
         BookStack.objects.filter(id=self.id).update(
@@ -168,19 +116,9 @@ class Category(models.Model):
         return self.category
 
 
-class BookStackCategoryManager(models.Manager):
-    def create_bookstack_category(self, bookstack, category):
-        bookstack_category = self.create(
-            bookstack=bookstack, category=category)
-        bookstack_category.save()
-        return bookstack_category
-
-
 class BookStackCategory(models.Model):
     bookstack = models.ForeignKey(BookStack, on_delete=models.PROTECT)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
-
-    objects = BookStackCategoryManager()
 
     class Meta:
         unique_together = ('category', 'bookstack')
