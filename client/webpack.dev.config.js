0f7fb97500b7d677aa3d03a73cd2515971294314
ebenpack <ebenpackwood@gmail.com>
2018-04-22 13:30:25 -0400

    Write template to disk & serve bundle files from dev server

--- client/webpack.dev.config.js
+++ client/webpack.dev.config.js
@@ -8,13 +8,16 @@ module.exports = {
     mode: 'development',
     plugins: [
         new DashboardPlugin(),
-        new WriteFilePlugin(),
+        new WriteFilePlugin({
+            test: /\.html$/,
+        }),
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         ...baseConfig.plugins,
     ],
     output: {
         ...baseConfig.output,
+        publicPath: "http://localhost:8080/static/bookstack/dist",
         filename: '[name].[hash].js',
     },
     devtool: 'source-map',
